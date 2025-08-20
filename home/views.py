2aeda4d17a2c1567102dbab95481a8feb2785873
Sourabh <saurabhmahawar10@gmail.com>
2025-08-20 19:08:27 +0530

    36004

--- home/views.py
+++ home/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
-def menu(request):
-    # Simple hardcoded menu list
-    menu_items = [
-        {"name": "Margherita Pizza", "price": 250},
-        {"name": "Paneer Butter Masala", "price": 300},
-        {"name": "Veg Biryani", "price": 200},
-        {"name": "Masala Dosa", "price": 150},
-        {"name": "Gulab Jamun", "price": 80},
-    ]
-    return render(request, 'menu.html', {"menu_items": menu_items})
+def contact(request):
+    contact_info = {
+        "phone": "+91 98765 43210",
+        "email": "info@myawesomerestaurant.com",
+        "address": "123 Food Street, Jaipur, Rajasthan, India"
+    }
+    return render(request, 'contact.html', {"contact": contact_info})
