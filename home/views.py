4236d629095725a44709cc7eb14b6ab744644954
Sourabh <saurabhmahawar10@gmail.com>
2025-08-20 19:05:04 +0530

    36002

--- home/views.py
+++ home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.conf import settings
 
 # Create your views here.
-def homepage(request):
-    return render(request, 'home.html', {
-        'restaurant_name': settings.RESTAURANT_NAME
-        'restaurant_phone': settings.RESTAURANT_PHONE
-    })
+def menu(request):
+    # Simple hardcoded menu list
+    menu_items = [
+        {"name": "Margherita Pizza", "price": 250},
+        {"name": "Paneer Butter Masala", "price": 300},
+        {"name": "Veg Biryani", "price": 200},
+        {"name": "Masala Dosa", "price": 150},
+        {"name": "Gulab Jamun", "price": 80},
+    ]
+    return render(request, 'menu.html', {"menu_items": menu_items})
