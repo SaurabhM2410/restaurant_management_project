b9195e24eb050b0ea0f3d5e005e806c2d8468a00
Sourabh <saurabhmahawar10@gmail.com>
2025-08-22 19:06:54 +0530

    36560

--- home/views.py
+++ home/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseServerError
+from django.db import DatabaseError
+from .models import MenuItem  # assuming you have a MenuItem model
 
-def contact(request):
-    contact_info = {
-        "phone": "+91 98765 43210",
-        "email": "info@myawesomerestaurant.com",
-        "address": "123 Food Street, Jaipur, Rajasthan, India"
-    }
-    return render(request, 'contact.html', {"contact": contact_info})
+def menu(request):
+    try:
+        items = MenuItem.objects.all()
+        return render(request, "menu.html", {"items": items})
+
+    except DatabaseError:
+        # If DB connection/query fails
+        return HttpResponseServerError("Sorry, we are having issues with the database. Please try again later.")
+
+    except Exception as e:
+        # Catch unexpected errors
+        return HttpResponse("An unexpected error occurred.", status=500)
