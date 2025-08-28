117b86043e0775260a248834a63fa5519962bc7a
Sourabh <saurabhmahawar10@gmail.com>
2025-08-28 17:39:29 +0530

    36556

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path
-from . import views
-
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('menu/', views.menu, name='menu'),
     path('contact/', views.contact, name='contact'),
+    path('reservations/', views.reservations, name='reservations'),  # <-- new
 ]
