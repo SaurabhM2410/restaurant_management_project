48c4b4bbe06677ee479e18ffe890d7b75065382e
Sourabh <saurabhmahawar10@gmail.com>
2025-10-19 20:25:59 +0530

    55487

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
+# orders/urls.py
+
 from django.urls import path
-from .views import *
+from .views import UserOrderHistoryView
 
 urlpatterns = [
-    
+    path('orders/history/', UserOrderHistoryView.as_view(), name='order-history'),
 ]
