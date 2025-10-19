f8dff94588c4108cc3b994b27d4f074654311335
Sourabh <saurabhmahawar10@gmail.com>
2025-10-19 20:20:13 +0530

    55491

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,12 @@
+# orders/models.py
+
 from django.db import models
 
-# Assume OrderStatus is already defined above
-class Order(models.Model):
-    # ... your existing fields ...
-    status = models.ForeignKey(
-        'OrderStatus',
-        on_delete=models.SET_NULL,
-        null=True
-    )
+class Coupon(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discount_percent = models.DecimalField(max_digits=5, decimal_places=2)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.code
